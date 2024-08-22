3256c55a7024db716782e7de20dd056eae4a17ae
Jacob Filik <jacob.filik@diamond.ac.uk>
2024-08-22 14:31:02 +0100

    tweak build script

--- scripts/build_database.py
+++ scripts/build_database.py
@@ -2,7 +2,7 @@ from larch.io import xdi
 from pathlib import Path
 import json
 
-path = "./xdidata"
+path = "./xdidata/data"
 
 element = "element"
 symbol = "symbol"
@@ -66,5 +66,5 @@ for child in p.rglob("*.xdi"):
         m.append(md)
 
 
-with open("./xdidata/db.json", 'w') as fh:
+with open("./xdidata/data/db.json", 'w') as fh:
     fh.write(json.dumps(m))
