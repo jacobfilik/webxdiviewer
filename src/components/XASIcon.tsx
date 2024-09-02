a62079c28c09e023008f4c398adc6629f274bf2d
Jacob Filik <jacob.filik@diamond.ac.uk>
2024-09-02 16:05:30 +0100

    update icon and title

---
+++ src/components/XASIcon.tsx
@@ -0,0 +1,44 @@
+import { SvgIcon } from "@mui/material";
+
+export default function XASIcon() {
+  return (
+    <SvgIcon fontSize="large">
+      <svg
+        xmlns:cc="http://creativecommons.org/ns#"
+        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        xmlns:svg="http://www.w3.org/2000/svg"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+        xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+        width="128mm"
+        height="128mm"
+        stroke="currentColor"
+        strokeWidth="10"
+        fill="currentColor"
+        fillOpacity={0}
+        viewBox="0 0 128 128"
+        version="1.1"
+        id="svg8"
+        inkscape:version="1.0.2 (e86c870879, 2021-01-15)"
+        sodipodi:docname="xas.svg"
+      >
+        <g inkscape:label="Layer 1" inkscape:groupmode="layer" id="layer1">
+          <ellipse
+            id="path10"
+            cx="63.999969"
+            cy="63.999977"
+            inkscape:transform-center-x="-5.865903"
+            inkscape:transform-center-y="5.0209768"
+            rx="56.583519"
+            ry="56.583527"
+          />
+          <path
+            d="m 16.735593,100.44088 c 0,0 7.70744,-0.26134 11.237986,-2.870607 3.530546,-2.609263 5.156198,-5.498236 7.679809,-12.583372 2.523611,-7.085136 2.848188,-18.380097 4.131012,-30.102996 1.282824,-11.722899 3.230562,-44.480225 3.230562,-44.480225 4.962867,19.231513 6.224793,38.773768 14.888602,57.69454 2.414322,5.272605 9.885109,-17.110327 17.038135,-18.175536 7.378059,-1.10638 11.361319,13.99222 22.096164,13.889823 7.447127,0.173557 14.641057,-5.092574 22.300757,-7.450702 v 0"
+            id="path835"
+            sodipodi:nodetypes="czzzcscccc"
+          />
+        </g>
+      </svg>
+    </SvgIcon>
+  );
+}
