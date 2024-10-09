cd0aba20384fc4f31b678f9d7d51bb4595bdd3ab
Jacob Filik <jacob.filik@gmail.com>
2024-10-09 15:25:13 +0100

    Add page and route for persitent view

---
+++ src/components/PersistentView.tsx
@@ -0,0 +1,68 @@
+
+import { useState, useContext } from "react";
+
+import axios from "axios";
+import MetadataTab from "./MetadataTab.tsx";
+
+import { Grid, Typography } from "@mui/material";
+import XDIFile from "../xdifile.ts";
+
+import { XDIFileProvider } from "../contexts/XDIFileContext.tsx";
+import XDIChart from "./XDIChart.tsx";
+
+import { useLocation} from "react-router-dom";
+import { useEffect } from "react";
+
+import { MetadataContext } from "../contexts/MetadataContext.tsx";
+
+
+
+function PersistentView() {
+
+    const location = useLocation();
+    const id = location.pathname.slice(5)
+
+    const [xdiFile, setXDIFile] = useState<XDIFile | null>(null);
+    const allStandards = useContext(MetadataContext);
+
+    console.log(id)
+    console.log(allStandards[1])
+
+    const standard = allStandards.find((f) => f.location === id)
+
+    useEffect(() => {
+        axios.get("/webxdiviewer/xdidata/" + id).then((response) => {
+        
+            let xdi = null;
+        try {
+            xdi = XDIFile.parseFile(response.data);
+
+        } catch {
+            console.log("Could not read {}", focus)
+
+        }
+        
+        setXDIFile(xdi);
+    })}, [id]);
+  
+
+  
+    // const onClick = getData();
+  
+    return (
+        <XDIFileProvider value={{ xdiFile: xdiFile, setXDIFile: setXDIFile }}>
+          <Grid height="100%" container>
+            <Grid item lg={5} md={12} padding={1}>
+            {standard 
+            ? <MetadataTab standard={standard} showDownload={true} />
+            : <Typography> Could not find {id} </Typography>
+            }
+            </Grid>
+            <Grid item height="100%" lg={7} md={12} padding={1}>
+              <XDIChart />
+            </Grid>
+          </Grid>
+        </XDIFileProvider>
+      );
+    }
+export default PersistentView;
